ca532a0c6b169e6904c2a8ed2b65db74c4b55dd3
santiagomurillo29 <smurillocano29@gmail.com>
2025-09-08 15:52:09 -0500

    implementation test jwt to domain and infrastructure

--- domain/usecase/src/main/java/co/com/bancolombia/usecase/exception/BusinessException.java
+++ domain/usecase/src/main/java/co/com/bancolombia/usecase/exception/BusinessException.java
@@ -1,6 +1,6 @@
 package co.com.bancolombia.usecase.user.exception;
 
-import co.com.bancolombia.model.user.globalmessage.GlobalMessage;
+import co.com.bancolombia.model.globalmessage.GlobalMessage;
 
 public class BusinessException extends CoreException{
     public BusinessException(GlobalMessage error) {
